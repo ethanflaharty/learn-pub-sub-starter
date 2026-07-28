c9e14b7e6ffbcee5f5dde708d1bcf4795edd157c
Ethan Flaharty <ethanflahartybusiness@gmail.com>
2026-07-28 12:23:22 -0500

    1. CH2 L3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("Error making connection: %v", err)
+	}
+
+	defer connection.Close()
+
+	fmt.Println("Connection was successful")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Shutting program down...")
+
+	defer connection.Close()
 }
