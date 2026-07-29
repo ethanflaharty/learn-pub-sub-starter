db083d60c2f6a47159aa155045b94e7ff472a17b
Ethan Flaharty <ethanflahartybusiness@gmail.com>
2026-07-29 12:21:28 -0500

    2. CH3 L1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -15,11 +17,24 @@ func main() {
 	connection, err := amqp.Dial(connectionString)
 	if err != nil {
 		log.Fatalf("Error making connection: %v", err)
+		return
 	}
 
 	defer connection.Close()
 
 	fmt.Println("Connection was successful")
+
+	cntnChan, err := connection.Channel()
+	if err != nil {
+		log.Fatalf("Error creating connection channel: %v", err)
+		return
+	}
+
+	err = pubsub.PublishJSON(cntnChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("Error publishing json: %v", err)
+		return
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
